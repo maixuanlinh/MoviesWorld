de334f10ae01a3ef04e28905f6b97bcda5d54759
maixuanlinh <maixuanlinhdkh@gmail.com>
2023-12-10 05:43:34 +0200

    modify the movies.js logic to show the movies from the corresponding theater, add search function on movies page

--- scripts/main.js
+++ scripts/main.js
@@ -1,73 +1,111 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetchTheaterAreas();
+document.addEventListener("DOMContentLoaded", function () {
+  fetchTheaterAreas();
 });
 
+
+
 function fetchTheaterAreas() {
-    const areasUrl = 'http://www.finnkino.fi/xml/TheatreAreas/';
-    fetch(areasUrl)
-        .then(response => response.text()) // Parse the XML text response
-        .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-        .then(data => {
-            const theaters = data.querySelectorAll('TheatreArea');
-            displayTheaters(theaters);
-        })
-        .catch(error => {
-            console.error('Error fetching theater areas:', error);
-        });
+  const areasUrl = "http://www.finnkino.fi/xml/TheatreAreas/";
+  fetch(areasUrl)
+    .then((response) => response.text()) // Parse the XML text response
+    .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then((data) => {
+      const theaters = data.querySelectorAll("TheatreArea");
+      displayTheaters(theaters);
+    })
+    .catch((error) => {
+      console.error("Error fetching theater areas:", error);
+    });
 }
 
 const theaterImageMapping = {
-    'Pääkaupunkiseutu': 'KinopalatsiHki_1920.jpg', 
-    'Espoo': 'Omena_1920.jpg', 
-    'Espoo: OMENA': 'Omena_1920.jpg',
-    'Espoo: SELLO': 'Sello_1920.jpg',
-    'Helsinki': 'KinopalatsiHki_1920.jpg',
-    'Helsinki: ITIS': 'Itis_1920x800.jpg',
-    'Helsinki: KINOPALATSI': 'KinopalatsiHki_1920.jpg',
-    'Helsinki: MAXIM': 'Maxim_1920a.jpg',
-    'Helsinki: TENNISPALATSI': 'Tennispalatsi_1920u.jpg', 
-    'Vantaa: FLAMINGO': 'Flamingo_1920b.jpg',
-    'Jyväskylä: FANTASIA': 'Fantasia_1920uu.jpg', 
-    'Kuopio: SCALA': 'Scala_1920a.jpg',
-    'Lahti: KUVAPALATSI': 'Kuvapalatsi_1920.jpg',
-    'Lappeenranta: STRAND': 'Strand_1920.jpg',
-    'Oulu: PLAZA': 'Plaza_1920a.jpg',
-    'Pori: PROMENADI': 'Pori2018_1920.jpg', 
-    'Tampere': 'Plevna_1920.jpg',
-    'Tampere: CINE ATLAS': 'Tre_CineAtlas_2019_1920.jpg', 
-    'Tampere: PLEVNA': 'Plevna_1920.jpg',
-    'Turku ja Raisio': 'mylly_luxe_entrance.jpg', 
-    'Turku: KINOPALATSI': 'KinopalatsiTurkuB_1920.jpg',
-    'Raisio: LUXE MYLLY': 'mylly_luxe_entrance.jpg' 
+  Pääkaupunkiseutu: "KinopalatsiHki_1920.jpg",
+  Espoo: "Omena_1920.jpg",
+  "Espoo: OMENA": "Omena_1920.jpg",
+  "Espoo: SELLO": "Sello_1920.jpg",
+  Helsinki: "KinopalatsiHki_1920.jpg",
+  "Helsinki: ITIS": "Itis_1920x800.jpg",
+  "Helsinki: KINOPALATSI": "KinopalatsiHki_1920.jpg",
+  "Helsinki: MAXIM": "Maxim_1920a.jpg",
+  "Helsinki: TENNISPALATSI": "Tennispalatsi_1920u.jpg",
+  "Vantaa: FLAMINGO": "Flamingo_1920b.jpg",
+  "Jyväskylä: FANTASIA": "Fantasia_1920uu.jpg",
+  "Kuopio: SCALA": "Scala_1920a.jpg",
+  "Lahti: KUVAPALATSI": "Kuvapalatsi_1920.jpg",
+  "Lappeenranta: STRAND": "Strand_1920.jpg",
+  "Oulu: PLAZA": "Plaza_1920a.jpg",
+  "Pori: PROMENADI": "Pori2018_1920.jpg",
+  "Tampere": "Plevna_1920.jpg",
+  "Tampere: CINE ATLAS": "Tre_CineAtlas_2019_1920.jpg",
+  "Tampere: PLEVNA": "Plevna_1920.jpg",
+  "Turku ja Raisio": "mylly_luxe_entrance.jpg",
+  "Turku: KINOPALATSI": "KinopalatsiTurkuB_1920.jpg",
+  "Raisio: LUXE MYLLY": "mylly_luxe_entrance.jpg",
 };
 
-
-
 function displayTheaters(theaters) {
-    const theatersContainer = document.getElementById('theaters-container');
-    
-    theaters.forEach(theater => {
-        const id = theater.getElementsByTagName('ID')[0].textContent;
-        const name = theater.getElementsByTagName('Name')[0].textContent;
+  const theatersContainer = document.getElementById("theaters-container");
 
-        // Map the theater name to the corresponding image filename
-        const imageName = theaterImageMapping[name] || 'default_image.jpg'; // Fallback to a default image if no match is found
+  theaters.forEach((theater) => {
+    const id = theater.getElementsByTagName("ID")[0].textContent;
+    const name = theater.getElementsByTagName("Name")[0].textContent;
 
-        const theaterElement = document.createElement('div');
-        theaterElement.className = 'theater-card';
-        theaterElement.innerHTML = `
+    // Map the theater name to the corresponding image filename
+    const imageName = theaterImageMapping[name] || "default_image.jpg"; // Fallback to a default image if no match is found
+
+    const theaterElement = document.createElement("div");
+    theaterElement.className = "theater-card";
+    theaterElement.innerHTML = `
             <img src="assets/${imageName}" alt="${name}" class="theater-image">
             <div class="theater-info">
-                <h2>${name}</h2>
-                <button onclick="selectTheater('${id}')">See Showtimes</button>
+                <h2>${name}</h2>              
             </div>
+            <div class="theater-button-div">
+            <button class="current-movies-btn" onclick="selectTheater('${id}', '${name}')">
+            <img src="assets/icons/film-strip.png" alt="Film Strip" class="film-strip-icon">
+            Current Movies</button>
+            </div>
+          
         `;
 
-        theatersContainer.appendChild(theaterElement);
-    });
+    theatersContainer.appendChild(theaterElement);
+  });
 }
-function selectTheater(theaterId) {
+function selectTheater(theaterId, theaterName) {
+    // Encode the theaterName to ensure the URL is valid
+    const encodedTheaterName = encodeURIComponent(theaterName);
+    
+    // Redirecting to the movies page with the selected theaterId and theaterName as parameters
+    window.location.href = `movies.html?theater=${theaterId}&theaterName=${encodedTheaterName}`;
+  }
+  
 
-    //redirecting to the movies page with the selected theaterId as a parameter
-    window.location.href = 'movies.html?theater=' + theaterId;
+function filterTheaters() {
+    const searchQuery = document.getElementById('theater-search-bar').value.toLowerCase();
+    const theaterCards = document.querySelectorAll('.theater-card');
+    const searchResultInfo = document.getElementById('search-result-info');
+
+    let visibleTheatersCount = 0;
+
+    theaterCards.forEach(card => {
+        const theaterName = card.querySelector('.theater-info h2').textContent.toLowerCase();
+        if (theaterName.includes(searchQuery)) {
+            card.style.display = ''; // Show the theater card
+            visibleTheatersCount++;
+        } else {
+            card.style.display = 'none'; // Hide the theater card
+        }
+    });
+
+    // Update the search result info text
+    if (searchResultInfo) {
+        searchResultInfo.textContent = `${visibleTheatersCount} theaters found.`;
+    } else {
+        const infoText = document.createElement('h2');
+        infoText.id = 'search-result-info';
+        infoText.textContent = `${visibleTheatersCount} theaters found.`;
+        document.getElementById('theater-search-container').appendChild(infoText);
+    }
 }
+
+document.getElementById('theater-search-bar').addEventListener('input', filterTheaters);
--- scripts/movies.js
+++ scripts/movies.js
@@ -1,16 +1,63 @@
-// List of example movie titles to display
-const movieTitles = [
-    'The Shawshank Redemption',
-    'The Godfather',
-    'The Dark Knight',
-    'The Godfather: Part II',
-    '12 Angry Men',
-    'Schindler\'s List',
-    'The Lord of the Rings: The Return of the King',
-    'Pulp Fiction',
-    'The Good, the Bad and the Ugly',
-    'The Lord of the Rings: The Fellowship of the Ring'
-];
+
+document.addEventListener('DOMContentLoaded', function() {
+    const params = new URLSearchParams(window.location.search);
+    const theaterId = params.get('theater');
+    const theaterName = params.get('theaterName'); 
+    if (theaterId, theaterName) {
+        fetchMoviesForTheater(theaterId, theaterName);
+    }
+});
+
+// Set to keep track of titles already fetched
+const fetchedTitles = new Set();
+
+function fetchMoviesForTheater(theaterId, theaterName) {
+    // Fetch the theater name and set it to the #theater-showing-text element
+    const theaterAreasUrl = `http://www.finnkino.fi/xml/TheatreAreas/`;
+    fetch(theaterAreasUrl)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+           
+            const theaterNameSpan = document.createElement('span');
+            theaterNameSpan.textContent = theaterName;
+            theaterNameSpan.classList.add('theater-name-style'); // Add a class for styling
+            
+            // Clear the existing text content before appending new content
+            document.getElementById('theater-showing-text').textContent = 'Currently showing at:  ';
+            document.getElementById('theater-showing-text').appendChild(theaterNameSpan);
+                    })
+        .catch(error => {
+            console.error('Error fetching theater name:', error);
+        });
+
+    // Continue with fetching the movies
+    const scheduleUrl = `http://www.finnkino.fi/xml/Schedule/?area=${theaterId}&dt=${getDate()}`;
+    fetch(scheduleUrl)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            const shows = data.querySelectorAll("Show");
+            shows.forEach(show => {
+                const title = show.querySelector("Title").textContent;
+                if (!fetchedTitles.has(title)) {
+                    fetchedTitles.add(title);
+                    fetchMovieDetails(title);
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching schedule:', error);
+        });
+}
+
+function getDate() {
+    const date = new Date();
+    return date.toISOString().split('T')[0]; // Format the date as yyyy-mm-dd
+}
+
+
+
 
 // Function to fetch movie details from the OMDB API
 function fetchMovieDetails(movieTitle) {
@@ -29,18 +76,29 @@ function fetchMovieDetails(movieTitle) {
 
 // Function to display movie details on the page
 function displayMovie(movieData) {
-    const moviesContainer = document.getElementById('movies-container');
+    // Check if the movie data is valid before creating the card
+    if (!movieData || movieData.Response === "False") {
+        console.log(`Movie data not found for: ${movieData.Title}`);
+        return; // Exit the function if movie data is not valid
+    }
 
+    const moviesContainer = document.getElementById('movies-container');
     const movieElement = document.createElement('div');
     movieElement.className = 'movie-card';
     movieElement.innerHTML = `
-        <img src="${movieData.Poster}" alt="${movieData.Title}">
+        <img src="${movieData.Poster !== "N/A" ? movieData.Poster : 'path/to/default/image.jpg'}" alt="${movieData.Title}">
         <div class="movie-info">
-            <h3>${movieData.Title}</h3>
-            <p>${movieData.Plot}</p>
+            <h3 class="movie-title">${movieData.Title}</h3>
+            <p class="movie-plot">${movieData.Plot}</p>
             <p><strong>Rating:</strong> ${movieData.imdbRating}</p>
-            <button>Add to Watchlist</button>
-            <button>Trailer</button>
+            <p><strong>Duration:</strong> ${movieData.Runtime}</p>
+            <p><strong>Released:</strong> ${movieData.Released}</p>
+            <div class="ticket-btn-container">   <button class="movie-tickets-btn" onClick="alert('This feature is being updated. Please check back later');">Buy tickets</button>   </div>
+            
+
+            
+          
+    
         </div>
     `;
     moviesContainer.appendChild(movieElement);
@@ -55,3 +113,27 @@ function showMoviesList() {
 
 // Call this function when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', showMoviesList);
+
+
+document.getElementById('search-bar').addEventListener('input', filterMovies);
+
+function filterMovies() {
+    const searchQuery = document.getElementById('search-bar').value.toLowerCase();
+    const movieCards = document.querySelectorAll('.movie-card');
+    const movieSearchResultInfo = document.getElementById('movie-search-result-info');
+    
+    let visibleMoviesCount = 0;
+    
+    movieCards.forEach(card => {
+        const title = card.querySelector('.movie-title').textContent.toLowerCase();
+        if (title.includes(searchQuery)) {
+            card.style.display = ''; // Show the movie card
+            visibleMoviesCount++;
+        } else {
+            card.style.display = 'none'; // Hide the movie card
+        }
+    });
+    
+    // Update the search result info text
+    movieSearchResultInfo.textContent = `Found ${visibleMoviesCount} movies.`;
+}
